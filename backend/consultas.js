65f10f66915ba97766aa66dae2701141aa0da396
miguecastrosanchez <migue.castro.sanchez@gmail.com>
2026-09-06 18:50:58 -0300

    consultas.js
    agregamos las consultas para el post con la consulta select*from posts orderr by id y para insertar el posrt con insert into con el postgre

--- backend/consultas.js
+++ backend/consultas.js
@@ -8,6 +8,8 @@ const pool = new Pool({
   allowExitOnIdle: true,
 });
 
+
+//probamos la conexion
 const probarConexion = async () => {
   
     const resultado = await pool.query("SELECT NOW()");
@@ -15,3 +17,32 @@ const probarConexion = async () => {
 };
 
 probarConexion();
+
+//funcion para consultar los POST
+const obtenerPosts = async () => {
+  const { rows } = await pool.query(
+    "SELECT * FROM posts ORDER BY id"
+  );
+
+  return rows;
+};
+
+//funcion para guardar un POST
+const agregarPost = async (titulo, url, descripcion) => {
+  
+    const consulta = `
+    
+    INSERT INTO posts (titulo, img, descripcion, likes)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+
+  `;
+
+  const valores = [titulo, url, descripcion, 0];
+
+  const { rows } = await pool.query(consulta, valores);
+
+  return rows[0];
+};
+
+module.exports = { obtenerPosts, agregarPost };
